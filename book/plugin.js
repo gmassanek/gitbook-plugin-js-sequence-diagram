7cdb40e48d4233dac1ab1a08db2a8f7da926acbc
Geoff Massanek <geoff@groupon.com>
2014-11-26 15:54:48 -0600

    Add scrolling wrapper

--- book/plugin.js
+++ book/plugin.js
@@ -2,6 +2,8 @@ require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
     element = $('code.lang-sequence');
     element.sequenceDiagram({theme: 'simple'});
-    element.parent().replaceWith(element.html());
+    wrapper = $("<div class='scroll'></div>");
+    wrapper.html(element.html());
+    element.parent().replaceWith(wrapper);
   });
 });
