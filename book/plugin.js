9d58cb01727af00350cba1ce28783e502594d0b2
Geoff Massanek <geoff@groupon.com>
2014-11-26 15:59:32 -0600

    Use vars so you can have multiple graphs

--- book/plugin.js
+++ book/plugin.js
@@ -1,8 +1,8 @@
 require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
-    element = $('code.lang-sequence');
+    var element = $('code.lang-sequence');
     element.sequenceDiagram({theme: 'simple'});
-    wrapper = $("<div class='scroll'></div>");
+    var wrapper = $("<div class='scroll'></div>");
     wrapper.html(element.html());
     element.parent().replaceWith(wrapper);
   });
